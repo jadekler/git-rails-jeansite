dfb8a11f5dbc25cce69d9edd79febbcbc2a403c7
Jean de Klerk <jadekler@gmail.com>
2014-12-16 17:38:15 -0700

    Added font-awesome, image url, and default image

---
+++ db/migrate/20141216212540_add_image_url.rb
@@ -0,0 +1,5 @@
+class AddImageUrl < ActiveRecord::Migration
+  def change
+    add_column :projects, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215212640) do
+ActiveRecord::Schema.define(version: 20141216212540) do
 
   create_table "blogs", force: true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141215212640) do
     t.datetime "updated_at"
     t.string   "github_url"
     t.string   "title"
+    t.string   "image_url"
   end
 
 end
