1e64e927c82f64164b4869a9f4a93a5deaeb2d06
Jean de Klerk <jadekler@gmail.com>
2014-12-17 07:02:51 -0700

    Fixed blog link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'blogs#index'
 
-  get '/blog', to: 'blogs#index'
   get '/blogs', to: 'blogs#index'
-  get '/blog/:id', to: 'blogs#show'
   get '/blogs/:id', to: 'blogs#show'
 
   get '/projects', to: 'projects#index'
