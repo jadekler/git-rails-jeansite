39de3307f00c139ee3cdff78687fc2100bc4b1f8
Jean de Klerk <jadekler@gmail.com>
2014-12-20 23:38:35 -0700

    Working conversion of old post using created_at and datetime class.

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,6 @@ Nam simul mandamus te, sed ei vocent tibique gubergren. Vis te zril apeirian pri
 
 Ei aliquam vocibus per, te nec impetus vidisse instructior, vel insolens deserunt consetetur ea. Has ea nihil meliore sapientem. Ius no ridens tincidunt argumentum. Vim an omnesque adversarium. Erant omnes semper nam ex, ei alia tantas has. His mutat nostrud prodesset ei, ut nec causae epicurei fabellas. Tollit appellantur vis et, omittam comprehensam has cu, qui tibique facilisi electram te.
 '},
-        {title: 'Moo Foo Boo', short: 'Lorem ipsum something something I dont remember how this goes.', content: '
-Lorem ipsum dolor sit amet, mea similique adolescens ex, movet splendide cu cum, ex alii malis assum usu. Pro in tota salutatus, eu mel volumus oportere. Eum iriure scripta complectitur eu, idque fabulas id eum. Ei quo ornatus propriae efficiendi, civibus hendrerit mea eu. Cu eos mutat iuvaret. Vim ei nulla errem tempor, per recusabo maiestatis te, rebum nulla no nec.
-
-Usu debet ridens menandri at, discere invenire usu at, percipitur scripserit ut pro. Ne est prima velit. Vidit causae blandit vel in. Ad magna convenire eam, cu euripidis consetetur vel, in exerci expetendis quo. Mea cu latine albucius, ut qui nusquam praesent imperdiet.
-
-Quod assentior an nam, qui placerat molestiae instructior id. Est ei appetere definiebas neglegentur, ex munere consetetur efficiendi vis. Ex autem apeirian repudiare sit. Eu quo appareat tacimates, graeci omittam perfecto ei vel, in cum prompta graecis.
-
-Nam simul mandamus te, sed ei vocent tibique gubergren. Vis te zril apeirian principes, at nisl percipit sed, qui veniam graeco eu. Ne case nullam alienum mea, mea et docendi vituperata, ne albucius moderatius efficiantur quo. Ex brute fuisset temporibus sed, case officiis expetenda ex eam. Sit alia equidem cu, quo cu sumo docendi, sit diam brute omittantur ex. Movet iudicabit vituperata eu est. Eu praesent persecuti accommodare vim, usu autem partem ea, dicam partiendo disputationi sit ei.
-
-Ei aliquam vocibus per, te nec impetus vidisse instructior, vel insolens deserunt consetetur ea. Has ea nihil meliore sapientem. Ius no ridens tincidunt argumentum. Vim an omnesque adversarium. Erant omnes semper nam ex, ei alia tantas has. His mutat nostrud prodesset ei, ut nec causae epicurei fabellas. Tollit appellantur vis et, omittam comprehensam has cu, qui tibique facilisi electram te.
-'}
+        {title: 'Hello World', created_at: DateTime.parse('28-12-2013'), content: 'This blog was built to learn Go but also to post things that I pick up along the way. I believe strongly that programmers as a whole benefit from a community of giving, not just one of taking.
+Therefore, I hope that you - the reader - finds something here of worth. And if nothing else, go fork my repo for this site (https://github.com/jadekler/git-go-jeansite) and start your own blog!'}
 ])
