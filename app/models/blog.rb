1de7c1a2779b35a7e3ac0e98969adbf7a6c8192f
Jean de Klerk <jadekler@gmail.com>
2014-12-31 19:00:11 +0200

    Fixed blog smart short to only concat before markdown character. Todo: smarten that up a bit so that we dont have tiny shorts here and there.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,9 @@ class Blog < ActiveRecord::Base
         if self.short
             self.short
         else
-            self.content[0..100]
+            first_markdown = self.content.index(/[`\[\(]/)
+            end_character = first_markdown && first_markdown < 100 ? first_markdown-1 : 100
+            self.content[0..end_character]
         end
     end
 end
