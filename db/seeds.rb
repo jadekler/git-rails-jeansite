fa55940d1c36d2d8e662419d16cc5c74c1301137
Jean de Klerk <jadekler@gmail.com>
2015-01-04 07:50:59 +0200

    Added real projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,20 @@
 projects = Project.create([
-        {title: 'Boom Projecto', github_url: 'http://boom.com'},
-        {title: 'Yolo swaggins', github_url: 'http://boom.com'},
-        {title: 'Boom Amigos', github_url: 'http://boom.com'},
-        {title: 'Several Projects', github_url: 'http://boom.com'},
-        {title: 'Heyo Projecto', github_url: 'http://boom.com'},
-        {title: 'Bam Project', github_url: 'http://boom.com'}
-])
+        {title: 'Web Attributes Demo', github_url: 'https://github.com/jadekler/git-web-attrdemo'},
+        {title: 'Require.js Demo', github_url: 'https://github.com/jadekler/git-web-requiredemo'},
+        {title: 'Page Change Checking App', github_url: 'https://github.com/jadekler/git-web-dataformatter'},
+        {title: 'Ember.js Treetable', github_url: 'https://github.com/jadekler/git-ember-treetable'},
+        {title: 'Ember.js Treetable with Zend', github_url: 'https://github.com/jadekler/git-zend-ember-jeansite'},
+        {title: 'jQuery Treetable', github_url: 'https://github.com/jadekler/git-jquery-treetable'},
+        {title: 'Ember.js Widget Demo', github_url: 'https://github.com/jadekler/git-ember-widgetdemo'},
+        {title: 'Ember.js Todo Demo', github_url: 'https://github.com/jadekler/git-ember-todo'},
+        {title: 'Solarized Theme for Chrome Devtools', github_url: 'https://github.com/jadekler/git-solarized-chrome-devtools'},
+        {title: 'Swift Todo Tutorial', github_url: 'https://github.com/jadekler/git-swift-todo-tutorial'},
+        {title: 'HRT-SQL Lexer in Go', github_url: 'https://github.com/jadekler/git-go-logictree'},
+        {title: 'Grunt Go Runner', github_url: 'https://github.com/jadekler/git-grunt-gorun'},
+        {title: 'Grunt Go Stopper', github_url: 'https://github.com/jadekler/git-grunt-gostop'},
+        {title: 'Grunt Go Builder', github_url: 'https://github.com/jadekler/git-grunt-gobuild'},
+        {title: 'Go Website Skeleton', github_url: 'https://github.com/jadekler/git-go-websiteskeleton'}
+].reverse)
 
 @blogs = []
 
